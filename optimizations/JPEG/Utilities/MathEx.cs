e6725361216dd71495f83974fa81cc9bb1061497
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2021-04-04 23:52:19 +0500

    Got rid of LINQ in MathEx

--- optimizations/JPEG/Utilities/MathEx.cs
+++ optimizations/JPEG/Utilities/MathEx.cs
@@ -5,17 +5,31 @@ namespace JPEG.Utilities
 {
     public static class MathEx
     {
-        public static double Sum(int from, int to, Func<int, double> function)
-            => Enumerable.Range(from, to - from).Sum(function);
-
         public static double SumByTwoVariables(int from1, int to1, int from2, int to2, Func<int, int, double> function)
-            => Sum(from1, to1, x => Sum(from2, to2, y => function(x, y)));
+        {
+            var sum = 0.0;
+            for (var i = from1; i < to1; i++)
+            {
+                for (var j = from2; j < to2; j++)
+                {
+                    sum += function(i, j);
+                }
+            }
+
+            return sum;
+        }
 
         public static double LoopByTwoVariables(int from1, int to1, int from2, int to2, Action<int, int> function)
-            => Sum(from1, to1, x => Sum(from2, to2, y =>
+        {
+            for (var i = from1; i < to1; i++)
             {
-                function(x, y);
-                return 0;
-            }));
+                for (var j = from2; j < to2; j++)
+                {
+                    function(i, j);
+                }
+            }
+
+            return 0;
+        }
     }
 }
