006c457403922f9f1648a8061d068ef15bc3776c
CakeOrRiot <pozdnyakovmaks99@mail.ru>
2021-04-05 00:41:13 +0500

    Got rid of IEnumerableExtensions

---
+++ optimizations/JPEG/Utilities/ListExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JPEG.Utilities
+{
+    static class ListExtensions
+    {
+        public static T MinOrDefault<T>(this List<T> list, Func<T, int> selector)
+        {
+            if (list.Count == 0)
+                return default;
+            var minVal = int.MaxValue;
+            var minItem = default(T);
+            foreach (var item in list)
+            {
+                var value = selector(item);
+                if (value >= minVal) continue;
+                minItem = item;
+                minVal = value;
+            }
+
+            return minItem;
+        }
+
+        public static List<T> Without<T>(this List<T> list, T element)
+        {
+            var result = new List<T>();
+            foreach (var item in list)
+            {
+                if (!item.Equals(element))
+                    result.Add(item);
+            }
+
+            return result;
+        }
+    }
+}
